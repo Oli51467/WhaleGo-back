264912a09df6945dbf7a50e46e7a7d4c8ede1fcf
Oli51467 <1832297226@qq.com>
2022-12-21 12:56:03 +0800

    init

---
+++ src/main/java/com/sdu/kob/KobBackApplication.java
@@ -0,0 +1,13 @@
+package com.sdu.kob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KobBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdu/kob/KobBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdu.kob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KobBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
