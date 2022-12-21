b1ef80491d7ebc4bcb6b75f9f9a1ebf0cf79c7c3
Oli51467 <1832297226@qq.com>
2022-12-21 15:51:18 +0800

    feat: 支持跨域请求

--- src/main/java/com/sdu/kob/controller/IndexController.java
+++ src/main/java/com/sdu/kob/controller/IndexController.java
@@ -3,10 +3,16 @@ package com.sdu.kob.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class IndexController {
     @RequestMapping("/index")
-    public String getInfo() {
-        return "kk";
+    public Map<String, String> getInfo() {
+        Map<String, String> result = new HashMap<>();
+        result.put("name", "tiger");
+        result.put("rating", "1500");
+        return result;
     }
 }
