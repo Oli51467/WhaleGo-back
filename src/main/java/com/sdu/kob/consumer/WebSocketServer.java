67cd2d23fef5071883dd15a8f1d3018e0af941b8
Oli51467 <1832297226@qq.com>
2022-12-25 14:06:38 +0800

    feat: 配置websocket

---
+++ src/main/java/com/sdu/kob/consumer/WebSocketServer.java
@@ -0,0 +1,78 @@
+package com.sdu.kob.consumer;
+
+import com.sdu.kob.domain.User;
+import com.sdu.kob.repository.UserDAO;
+import com.sdu.kob.service.impl.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@ServerEndpoint("/websocket/{token}")  // 注意不要以'/'结尾
+public class WebSocketServer {
+
+    // 存储所有user对应的连接 当匹配成功后，将匹配成功的连接返回给用户 加static为的是users对所有实例均可见
+    private static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
+    private User user;
+    private Session session = null; // 用户信息存储到session中
+
+    private static UserDAO userDAO; // 用静态变量的set函数注入
+
+    @Autowired
+    public void setUserMapper(UserDAO userDAO) {
+        WebSocketServer.userDAO = userDAO;
+    }
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("token") String token) {
+        // 建立连接 所有与连接相关的信息都会存到这个类中
+        this.session = session;
+        System.out.println("Connected!");
+        // 1. 从token中读取建立连接的用户是谁
+
+        // 2. 取出userId
+        int userId = Integer.parseInt(token);
+        this.user = userDAO.findById(userId);
+        // 3. 将用户存下来
+        users.put(userId, this);
+    }
+
+    @OnClose
+    public void onClose() {
+        // 关闭连接
+        System.out.println("Closed!");
+        if (this.user != null) {
+            users.remove(this.user.getId());
+        }
+    }
+
+    @OnMessage
+    // 从Client接收消息 接收到前端信息时触发
+    public void onMessage(String message, Session session) {
+        System.out.println("Receive message");
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+    // 从后端向前端发送信息
+    public void sendMessage(String message) {
+        // 每个连接用Session维护
+        synchronized (this.session) {
+            try {
+                this.session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
